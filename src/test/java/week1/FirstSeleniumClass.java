048c22a2e6f772f4fe884339adc4825565d9868c
dozerozturk <ozturkozer@yahoo.com>
2023-01-07 14:53:52 -0600

    First Commit

--- src/test/java/week1/FirstSeleniumClass.java
+++ src/test/java/week1/FirstSeleniumClass.java
@@ -1,19 +1,23 @@
 package week1;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.edge.*;
+import org.openqa.selenium.firefox.*;
 
 public class FirstSeleniumClass {
 
     public static void main(String[] args) {
 
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://practice.cydeo.com");
+        WebDriverManager.edgedriver().setup();
 
+        WebDriver driverChrome = new ChromeDriver();
+        driverChrome.get("https://practice.cydeo.com/");
 
+        WebDriver driverEdge = new EdgeDriver();
+        driverEdge.get("https://www.google.com");
 
 
     }
